32c9399ccee99ace59bdbd087530ff2d13eae596
dksifoua <dimitri.sifoua@gmail.com>
2026-01-19 01:10:06 -0500

    Add SearchContainer component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { type JSX } from "react"
 import { Header } from "@/components/Header"
+import { SearchContainer } from "@/components/SearchContainer"
 
 export function App(): JSX.Element {
 
@@ -7,6 +8,9 @@ export function App(): JSX.Element {
         <>
             <Header/>
             <p className="text-preset-2 text-center md:w-96 lg:w-183 md:m-auto">How's the sky looking today?</p>
+            <main className="flex flex-col gap-y-8 lg:gap-y-12">
+                <SearchContainer/>
+            </main>
         </>
     )
 }
---
+++ src/components/SearchContainer.tsx
@@ -0,0 +1,15 @@
+import type { JSX } from "react"
+import SearchIcon from "@/assets/images/icon-search.svg"
+
+export function SearchContainer(): JSX.Element {
+
+    return (
+        <form className="md:w-164 flex flex-col md:flex-row gap-y-3 md:gap-x-4 md:m-auto">
+            <div className="h-14 md:w-full flex flex-row gap-x-4 px-6 py-4 items-center justify-between bg-neutral-800 rounded-12">
+                <img src={SearchIcon} alt="Search Icon" className="w-5 h-5"/>
+                <input type="search" placeholder="Search for a place..." className="w-full focus:outline-none placeholder:text-preset-5 color-neutral-200"/>
+            </div>
+            <button type="submit" className="h-14 px-6 py-4 rounded-12 bg-blue-500 text-preset-5">Search</button>
+        </form>
+    )
+}
