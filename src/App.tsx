5208e47eb8482e829a0c2df5d1634159f76eab2e
dksifoua <dimitri.sifoua@gmail.com>
2026-01-19 00:40:44 -0500

    Add Title component.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ export function App(): JSX.Element {
     return (
         <>
             <Header/>
+            <p className="text-preset-2 text-center md:w-96 lg:w-183 md:m-auto">How's the sky looking today?</p>
         </>
     )
 }
